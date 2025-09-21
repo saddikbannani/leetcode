f3a4c97051f525784ae84ba0407406feba3accf1
Essaddik Bannani <54484355+saddikbannani@users.noreply.github.com>
2025-09-21 10:39:10 +0200

    Time: 80 ms (5.07%), Space: 56.6 MB (83.32%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,12 @@
+var removeDuplicates = function(nums) {
+    let numsLength = nums.length;
+    for (let i = 0; i < numsLength; i++) {
+        if (i >= nums.length) {
+            break;
+        }
+        if (nums[i] === nums[i + 1]) {
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+};
