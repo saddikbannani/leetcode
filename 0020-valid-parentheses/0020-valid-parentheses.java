c3d19be70b5b2d94476e63d282fa5f43af979aef
Essaddik Bannani <54484355+saddikbannani@users.noreply.github.com>
2025-10-22 16:40:23 +0200

    Time: 3 ms (58.78%), Space: 41.9 MB (74.62%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,23 @@
+	public class Solution {
+    public boolean isValid(String s) {
+        java.util.Map<Character, Character> hashmap = new java.util.HashMap<>();
+        hashmap.put(')', '(');
+        hashmap.put('}', '{');
+        hashmap.put(']', '[');
+        
+        Stack<Character> stk = new Stack<>();
+ 
+        for (char c : s.toCharArray()) {
+            if (!hashmap.containsKey(c)) {
+                stk.push(c);
+            } else {
+                if (stk.isEmpty() || stk.pop() != hashmap.get(c)) {
+                    return false;
+                }
+            }
+        }
+ 
+        return stk.isEmpty();
+    }
+}
+ 
